38bf3f7b2fb49a4538782dd127cd57200074df8d
Aziz-khan88 <aziz.rehman@crystallitedigital.com>
2025-01-16 15:44:09 +0500

    pirce changs

--- src/app/landing/paid/bannerlp.jsx
+++ src/app/landing/paid/bannerlp.jsx
@@ -14,7 +14,7 @@ const BannerLp = () => {
                     <Col md={6} lg={6} className="my-auto">
                         <div className={styles.bannerContentSec}>
                             <h1>Accelerate Your Growth with Data Driven Paid Marketing</h1>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type </p>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type </p>
                             <div className={styles.btnFlex}>
                                 <CommonBtn txt="Free Consultation" Livechat={true} />
                             </div>
